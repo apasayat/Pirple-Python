102923e1c39c646dbac4f097f57358a60fa5f323
Abhishek Pasayat <apasayat3@gmail.com>
2019-01-07 10:32:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# This is a homework assignment on variables.
+"""
+I need to list as many attributes as possible for any song that is my favorite.
+"""
+
+Song = "Tum Hi Ho"                #Name of the song
+Writer = "Sandeep Nath"           #Name of the writer
+Singer = "Arijit Singh"           #Name of the singer
+Genre = "Classic"                 #Genre of the song
+Duration = 4.22                   #Duration of the song in minutes and seconds. Its a Decimal value  
+ReleasedYear = 2013               #Year in which the song was released
+AssociatedFilm = "Aashiqui2"      #Film in which the song was present
+
+print(Song)
+print(Writer)
+print(Singer)
+print(Genre)
+print(Duration)
+print(ReleasedYear)
+print(AssociatedFilm)
+
+
+
+
+
+
